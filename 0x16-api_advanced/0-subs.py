e02cace8a94b1b1b13be2387cc918616d47bb7c4
Mkothm <mkothman.jr@gmail.com>
2023-08-08 17:31:12 +0000

    retruns number

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Number of subs """
+
+
+def number_of_subscribers(subreddit):
+	"""
+	Returns number of sub
+	"""
+	import requests
+
+	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+	headers = {"User-Agent": "Mkothm"}
+
+	response = requests.get(url, headers=headers, allow_redirects=False)
+
+	if response.status_code == 404:
+		return 0
+	else:
+		res = response.json().get("data")
+		return res.get("subscribers")
